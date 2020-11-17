dd613a25ec7da18ee61d2e36fc220d9375df5ca3
simon liu <simonlyllyyy@gmail.com>
2020-11-17 11:10:10 +1100

    add corner

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import Character from './components/character'
 function App() {
   return (
     <div className="frame">
+      <div className="corner_topleft"></div>
+      <div className="corner_topright"></div>
+      <div className="corner_bottomleft"></div>
+      <div className="corner_bottomright"></div>
+
       <div className="camera">
         <div className="map pixel-art">
           <Character />
